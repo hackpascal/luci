ca314cdb96c88216ee0e2912aab4bf004691b861
hackpascal <hackpascal@qq.com>
2016-04-15 20:10:32 +0800

    qca-wifi 使用国家代码列表

--- modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/wifi.lua
+++ modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/wifi.lua
@@ -254,10 +254,20 @@ if hwtype == "qcawifi" then
 		end
 	end
 
+	local cl = iw and iw.countrylist
+	if cl and #cl > 0 then
+		cc = s:taboption("advanced", ListValue, "country", translate("Country Code"), translate("Use ISO/IEC 3166 alpha2 country codes."))
+		cc.default = tostring(iw and iw.country or "00")
+		for _, c in ipairs(cl) do
+			cc:value(c.alpha2, "%s - %s" %{ c.alpha2, c.name })
+		end
+	else
+		s:taboption("advanced", Value, "country", translate("Country Code"), translate("Use ISO/IEC 3166 alpha2 country codes."))
+	end
+
 	s:taboption("advanced", Value, "txantenna", translate("Tx Antenna bitmask"))
 	s:taboption("advanced", Value, "rxantenna", translate("Rx Antenna bitmask"))
 	s:taboption("advanced", Value, "regdomain", translate("Regulatory Domain"))
-	s:taboption("advanced", Value, "country", translate("Country Code"))
 
 end
 
